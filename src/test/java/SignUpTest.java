43f52f3412c60f1c719d54b51c49a7898d2689a2
Judyta Odrobinska <judyta.odrobinska@gmail.com>
2024-08-03 23:20:24 +0200

    Add sign up test with invalid email

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -97,4 +97,42 @@ public class SignUpTest {
 
     }
 
+
+    @Test
+    public void signUpInvalidEmail() {
+
+        WebDriver driver = getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS); // oczekiwanie na elementy
+
+        // Sign Up
+        driver.findElements(By.xpath("//li[@id='li_myaccount']")).stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
+        driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+
+        // Filling the Form
+        String lastName = "Kowalska";
+
+        // Unikalny adres email
+        int randomNumber = (int) (Math.random()*1000); //numer
+        String email = "judit" + randomNumber; // niezgodny format e-mail'a
+
+        // Filling the Form
+        driver.findElement(By.name("firstname")).sendKeys("Katarzyna");
+        driver.findElement(By.name("lastname")).sendKeys(lastName);
+        driver.findElement(By.name("phone")).sendKeys("111222333");
+        driver.findElement(By.name("email")).sendKeys(email); // podajemy zmienną
+        driver.findElement(By.name("password")).sendKeys("Test123");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Test123");
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+
+        List<String> errors = driver.findElements(By.xpath("//div[@class='alert alert-danger']//p")).stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+
+        Assert.assertTrue(errors.contains("The Email field must contain a valid email address."));
+
+    }
+
 }
