4250934f35f23a6db5a9f4236b5c14eb3f58c7d6
Judyta Odrobinska <judyta.odrobinska@gmail.com>
2024-08-06 16:03:01 +0200

    Google Page Object Pattern

---
+++ src/test/java/AAGoogleHomePage.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AAGoogleHomePage {
+
+    @FindBy(name = "q")
+    private WebElement searchInput;
+
+    @FindBy(name = "btnk")
+    private WebElement searchButton;
+
+
+    public void searchInGoogle(String phrase) {
+        searchInput.sendKeys(phrase);
+        searchButton.click();
+    }
+}
---
+++ src/test/java/AAGoogleTest.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+
+public class AAGoogleTest {
+
+    @Test
+    public void googleSearchTest() {
+        AAGoogleHomePage googleHomePage = new AAGoogleHomePage();
+        googleHomePage.searchInGoogle("Selenium");
+    }
+
+    @Test
+    public void googleSearchTest2() {
+        AAGoogleHomePage googleHomePage = new AAGoogleHomePage();
+        googleHomePage.searchInGoogle("Selenium");
+    }
+
+    @Test
+    public void googleSearchTest3() {
+        AAGoogleHomePage googleHomePage = new AAGoogleHomePage();
+        googleHomePage.searchInGoogle("Selenium");
+    }
+
+}
